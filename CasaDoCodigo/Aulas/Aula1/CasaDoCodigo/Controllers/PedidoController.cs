408cf1b0889f743b471c8fa45b44bea7224f1f59
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-07-11 20:04:51 -0300

    ui project and assets incorporated

---
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Carrossel()
+        {
+            return View();
+        }        
+    }
+}
