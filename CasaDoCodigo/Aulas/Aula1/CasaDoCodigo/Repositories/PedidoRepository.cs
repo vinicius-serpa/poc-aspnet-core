1a476387312f5ea8f3ee70ac0d5609af73d9af20
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-08-01 23:06:54 -0300

    Include Cadastro into return of Pedido using linq

--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -60,6 +60,7 @@ namespace CasaDoCodigo.Repositories
             var pedido = dbSet
                 .Include(p => p.Itens)
                     .ThenInclude(i => i.Produto)
+                .Include(p => p.Cadastro)
                 .Where(p => p.Id == pedidoId)
                 .SingleOrDefault();
 
